ca4675bdb9c90d5691ed34f3cbf776414271c3d4
Andrea Simeoni <andrea.simeoni@bnlmail.com>
2019-04-21 21:00:21 +0200

    Hash strategy setup for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
